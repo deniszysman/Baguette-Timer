1294007b1ceaf7abd22923d7c3a09acca8e1547d
Denis Zysman <zysman.denis@gmail.com>
2025-12-02 17:04:28 -0800

    Initial Commit

---
+++ Baguette TimerTests/Baguette_TimerTests.swift
@@ -0,0 +1,16 @@
+//
+//  Baguette_TimerTests.swift
+//  Baguette TimerTests
+//
+//  Created by Denis Zysman on 12/2/25.
+//
+
+import Testing
+
+struct Baguette_TimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
